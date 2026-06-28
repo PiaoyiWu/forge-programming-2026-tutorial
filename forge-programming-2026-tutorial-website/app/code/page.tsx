1dfb7243b6e0fac0fcb94235724b350d715346d9
Piaoyi Wu <piaoyiwu1@gmail.com>
2026-06-28 12:25:02 -0600

    rn it reads one file and puts it on the codepage which is progress!! i think ill probably use one-light for light mode and github-dark for dark mode but this code is looking good!

--- forge-programming-2026-tutorial-website/app/code/page.tsx
+++ forge-programming-2026-tutorial-website/app/code/page.tsx
@@ -1,7 +1,20 @@
-export default function CodePage() {
+import fs from "fs";
+import path from "path";
+import { codeToHtml } from "shiki";
+
+export default async function CodePage() {
+
+    const turretShooterSubsystemPath = path.join(process.cwd(), "4421-robot-code-2026/main/java/frc/robot/subsystems/TurretShooterSubsystem.java");
+    const dispTurretShooterSubsystemCode = fs.readFileSync(turretShooterSubsystemPath, "utf-8");
+
+    const addTextColors = await codeToHtml(dispTurretShooterSubsystemCode, {lang: "java", theme: "one-light"});
+
     return (
         <main>
             <h1 className="font-bold p-4 text-5xl font-mono ml-5">CODE</h1>
+            <div className="ml-3">
+                <div dangerouslySetInnerHTML={{ __html: addTextColors }} />
+            </div>
         </main>
     );
 }
