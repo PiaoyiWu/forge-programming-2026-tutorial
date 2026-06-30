ca270a98f7d427ae714f7a2c0e471612a7468dcd
Piaoyi Wu <piaoyiwu1@gmail.com>
2026-06-29 20:07:14 -0600

    added line numbers by individually highlighting each line as a div, but lost the indentation from the <pre> tag i think

--- forge-programming-2026-tutorial-website/app/code/files/[filename]/page.tsx
+++ forge-programming-2026-tutorial-website/app/code/files/[filename]/page.tsx
@@ -1,7 +1,8 @@
 import fs from "fs";
 import path from "path";
 import {glob} from "glob";
-import {codeToHtml} from "shiki";
+import {codeToHast, codeToHtml} from "shiki";
+import {toHtml} from "hast-util-to-html";
 import FolderBar from "@/components/FolderBar"
 
 export default async function FilePage({params}: {params: Promise<{filename: string}>}) {
@@ -9,20 +10,60 @@ export default async function FilePage({params}: {params: Promise<{filename: str
 
     const findFile = await glob(`4421-robot-code-2026/robot/**/${filename}.java`, {cwd: process.cwd()});
 
+    if (findFile.length == 0) {
+        return (
+            <main>
+                <h1>404 not found</h1>
+            </main>
+        )
+    }
+
     const filePath = path.join(process.cwd(), findFile[0]);
         const dispFileCode = fs.readFileSync(filePath, "utf-8");
     
+        // returns js object (instead of <pre> and <span> tags it will return an object with tagname or type.)
+        // planning to use this so that you can manipulate each line so that you can add a class, line number, line id, or a comment
+        const createHast = await codeToHast(dispFileCode, 
+            {lang: "java", theme: "one-light"}
+        )
+
+
+        // currently the one in use
         const addTextColors = await codeToHtml(dispFileCode, {lang: "java", theme: "one-light"});
     
+
+        const preTag = createHast.children[0] as any;
+        const codeTag = preTag.children[0] as any;
+
+        //add a filter right here so that it ignores all the "text: \n" or new lines or wtv
+        const lines = codeTag.children.filter((node: any) => (node.type === "element" && node.tagName === "span")) as any[];
+
+
+        // 0number of lines
+        console.log(lines.length);
+
+        
         return (
             <main className="flex flex-col h-screen max-h-screen overflow-hidden">
-                <h1 className="font-bold p-4 text-5xl font-mono ml-5 flex-none">{filename}.java</h1>
+                <h1 className="font-bold p-5 text-5xl font-mono ml-5 flex-none">{filename}.java</h1>
                 <div className="min-h-0 flex flex-1 border-t border-black">
                     <div className="h-full overflow-y-auto flex-none">
                         <FolderBar />
                     </div>
                     
-                    <div className="h-full overflow-y-auto border-l border-black bg-[#FAFAFA] text-xs p-10 min-w-0 flex-1 overflow-x-auto [&_pre]:w-max [&_pre]:min-w-full" dangerouslySetInnerHTML={{ __html: addTextColors }} />
+                    <div className="h-full overflow-y-auto border-l border-black bg-[#FAFAFA] text-xs p-10 min-w-0 flex-1 overflow-x-auto [&_pre]:w-max [&_pre]:min-w-full">
+                        {lines.map((line, i) => {
+                            const lineNumber = i + 1;
+                            const lineToHTML = toHtml(line);
+
+                            return (
+                                <div key={lineNumber} data-lineNumber={lineNumber} className="flex">
+                                    <span>{lineNumber}</span>
+                                    <span dangerouslySetInnerHTML={{__html: lineToHTML}} />
+                                </div>
+                            );
+                        })}
+                    </div>
     
                 </div>
             </main>
